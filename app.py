8df0dcf2b5fd8df13aa18dbb6c83348c28e10935
System Administrator <root@nyabugandas-MacBook-Pro.local>
2020-12-03 19:21:50 -0700

    last commits

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import mariadb
 from flask import Flask, request, Response
 import json
 import dbcreds
-from flask_cors import CORS
+
 
    
 app = Flask(__name__)
-CORS(app)
+
 @app.route("/api/signup",methods=['POST'])
 def signup():
     if request.method == 'POST':
